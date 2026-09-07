9d63c7c39348de2f9918b25813812b7b06e46213
Nicolas Richard <theonewiththeevillook@yahoo.fr>
2026-09-07 17:08:36 +0200

    Update generate-ics-and-config-file.mjs: remove profs from event title (temp hack)

--- scripts/generate-ics-and-config-file.mjs
+++ scripts/generate-ics-and-config-file.mjs
@@ -150,7 +150,7 @@ function addEvent(listIn, map, icsEvent) {
  */
 function getIcsEvent(event, type) {
   const groupes = LIST_FORMATTER.format(event.groupes?.map(groupe => groupe.name))
-  const profs = LIST_FORMATTER.format(event.profs?.map(prof => prof.code))
+  const profs = "?" // LIST_FORMATTER.format(event.profs?.map(prof => prof.code))
   const salles = LIST_FORMATTER.format(event.salles?.map(salle => salle.name) || "")
   const aa = LIST_FORMATTER.format(event.cours?.map(cours => cours.name))
 
